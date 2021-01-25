12ecaf29744e983d29bfc1a21a56c42753de2a81
Gariel Mora <bryan95_19@hotmail.com>
2021-01-25 12:52:39 -0500

    Cambio de Modelos

--- donBosquito/src/main/java/ec/edu/ups/donBosquito/modelo/Valor_Poliza.java
+++ donBosquito/src/main/java/ec/edu/ups/donBosquito/modelo/Valor_Poliza.java
@@ -1,6 +1,7 @@
 package ec.edu.ups.donBosquito.modelo;
 
 import java.io.Serializable;
+import java.sql.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -19,6 +20,8 @@ public class Valor_Poliza implements Serializable {
 	private int valor_id;
 	private String plazo;
 	private Double tasa_interes;
+	private Date fechaInicio;
+	private Date fechaFinal;
 
 	public int getValor_id() {
 		return valor_id;
@@ -44,9 +47,26 @@ public class Valor_Poliza implements Serializable {
 		this.tasa_interes = tasa_interes;
 	}
 
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public Date getFechaFinal() {
+		return fechaFinal;
+	}
+
+	public void setFechaFinal(Date fechaFinal) {
+		this.fechaFinal = fechaFinal;
+	}
+
 	@Override
 	public String toString() {
-		return "Valor_Poliza [valor_id=" + valor_id + ", plazo=" + plazo + ", tasa_interes=" + tasa_interes + "]";
+		return "Valor_Poliza [valor_id=" + valor_id + ", plazo=" + plazo + ", tasa_interes=" + tasa_interes
+				+ ", fechaInicio=" + fechaInicio + ", fechaFinal=" + fechaFinal + "]";
 	}
 
 }
